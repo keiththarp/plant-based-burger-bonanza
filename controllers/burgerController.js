abcc3a07161d0422d66fd5167bd28568e81f8466
Keith Tharp <keithstharp@gmail.com>
2020-09-17 21:04:38 -0400

    Changing cats to burgers

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = express.Router();
-// Import the model (cat.js) to use its database functions.
+// Import the model (burger.js) to use its database functions.
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", async function (req, res) {
-  const hbsObject = { cats: await cat.all() };
+  const hbsObject = { burgers: await burger.all() };
   console.log(hbsObject);
   res.render("index", hbsObject);
 });
 
-router.post("/api/cats", async function (req, res) {
+router.post("/api/burgers", async function (req, res) {
   try {
-    const result = await cat.create(
+    const result = await burger.create(
       [
-        "name", "sleepy"
+        "name", "devoured"
       ],
       [
-        req.body.name, req.body.sleepy
+        req.body.name, req.body.devoured
       ]
     );
     // Send back the ID of the new quote
@@ -29,13 +29,13 @@ router.post("/api/cats", async function (req, res) {
   }
 });
 
-router.put("/api/cats/:id", async function (req, res) {
+router.put("/api/burgers/:id", async function (req, res) {
   const condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   try {
-    const result = await cat.update({ sleepy: req.body.sleepy }, condition);
+    const result = await burger.update({ devoured: req.body.devoured }, condition);
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
@@ -49,11 +49,11 @@ router.put("/api/cats/:id", async function (req, res) {
   }
 });
 
-router.delete("/api/cats/:id", async function (req, res) {
+router.delete("/api/burgers/:id", async function (req, res) {
   const condition = "id = " + req.params.id;
 
   try {
-    const result = await cat.delete(condition)
+    const result = await burger.delete(condition)
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
