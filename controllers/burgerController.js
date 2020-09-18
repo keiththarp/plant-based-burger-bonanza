24eafdc02a727076f142f1a09fadbf863e5c3af1
Keith Tharp <keithstharp@gmail.com>
2020-09-18 13:17:23 -0400

    updating paths

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 // Import the model (burger.js) to use its database functions.
-const burger = require("../models/burger.js");
+const burger = require("./models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", async function (req, res) {
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,5 @@
 // Import the ORM to create functions that will interact with the database.
-const dbManager = require("../config/burgerDbManager");
+const dbManager = require("./config/burgerDbManager");
 
 var burger = {
   all: function () {
