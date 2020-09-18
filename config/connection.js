42665c8f88a3a49169560f3923334a6becc1df7d
Keith Tharp <keithstharp@gmail.com>
2020-09-18 11:54:37 -0400

    updated server info for Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "<07KT&44kt>",
-  database: "veganBurger_db"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "<07KT&44kt>",
+    database: "veganBurger_db"
+  });
+};
 
 // Make connection.
 connection.connect(function (err) {
