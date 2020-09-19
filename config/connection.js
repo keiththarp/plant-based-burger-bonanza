6b97bccec001ff36b8c2724c4a6e6b96201a9c47
Keith Tharp <keithstharp@gmail.com>
2020-09-19 18:12:03 -0400

    updatin env vars

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root",
-    password: "<07KT&44kt>",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "veganBurger_db"
   });
 };
